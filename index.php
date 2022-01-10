9d875938dca2933f4ebc174a3d7c577b95eedec7
SinitsinAleksey <sinitsin.aleksey2003@yandex.ru>
2022-01-10 21:08:00 +0300

    Add basic layout

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="conent">
+    <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-6">
+            <form class="mt-5">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Фио</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Адрес проживания</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary">Подтвердить</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
